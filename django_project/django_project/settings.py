24fe6a2aeeb7f6813ec72f0296378d68b4fad490
Asyafa Ditra <asyafaditra@gmail.com>
2023-11-13 13:11:54 +0700

    create reset password feature

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -129,3 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'azaleapansy04@gmail.com'
+EMAIL_HOST_PASSWORD = 'sdrxcxrdwekmfgyb'
