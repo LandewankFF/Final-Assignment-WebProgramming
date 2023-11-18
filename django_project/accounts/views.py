7d835084ef8b019a11333879e2c3bbdaab34f900
Asyafa Ditra <asyafaditra@gmail.com>
2023-11-18 15:25:20 +0700

    connect frontend to backend

--- django_project/accounts/views.py
+++ django_project/accounts/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'authentication/signup.html'
+    template_name = 'authentication/login_register.html'
     
     def dispatch(self, request):
         if self.request.user.is_authenticated:
@@ -17,5 +17,5 @@ class SignUpView(CreateView):
     
 class CustomLoginView(LoginView):
     redirect_authenticated_user = True
-    template_name = 'authentication/login.html'
+    template_name = 'authentication/login_register.html'
 
