2304b0597abd39291c996054950165418abb6b1a
LandewankFF <landewankfahrezaf@gmail.com>
2023-11-30 10:01:42 +0700

    navbar responsive finish ready to use

--- src/script/menu.js
+++ src/script/menu.js
@@ -4,8 +4,3 @@ const menuList = document.getElementById('menu-list')
 hamburgerIcon.addEventListener('click', () => {
     menuList.classList.toggle('hidden')
 })
-
-
-// for put on search bar on navigation
-const searchNav = document.getElementById('search-nav')
-const wrappNav  = document.querySelector('.wrap-nav')
