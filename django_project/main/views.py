27f245f0639e35d5f2bc733e7e0f811d50aa22fd
LandewankFF <landewankfahrezaf@gmail.com>
2023-12-12 21:55:34 +0700

    delete

--- django_project/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-
-# Create your views here.
-def test(request):
-    return render(request=request, template_name='test.html')
--- django_project/static_collection/js/home/menu.js
+++
@@ -1,7 +0,0 @@
-// for create humburger menu
-const hamburgerIcon = document.getElementById('hamburger-menu');
-const menuList = document.getElementById('menu-list')
-
-hamburgerIcon.addEventListener('click', () => {
-    menuList.classList.toggle('active');
-});
