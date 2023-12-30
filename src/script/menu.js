8b5c97c314e7bbf1bc9748000580d3510da8d2ba
LandewankFF <landewankfahrezaf@gmail.com>
2023-12-30 14:27:37 +0700

    fix function call

--- src/script/menu.js
+++ src/script/menu.js
@@ -5,3 +5,15 @@ const menuList = document.getElementById('menu-list')
 hamburgerIcon.addEventListener('click', () => {
     menuList.classList.toggle('active');
 });
+
+// function emenrgency call
+const emergencyCall = document.getElementById('emergencyCall');
+
+        if (emergencyCall) {
+            emergencyCall.style.cursor = 'pointer'
+            emergencyCall.addEventListener('click', function () {
+                const phoneNumber = '+6282112706323';
+
+                window.location.href = 'tel:' + phoneNumber;
+            });
+        }
