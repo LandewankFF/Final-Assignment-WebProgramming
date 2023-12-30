f212fcea71fcea6b8a5f6883d1a3fa2a47e657d8
LandewankFF <landewankfahrezaf@gmail.com>
2023-12-30 15:38:18 +0700

    until FAQ

--- src/script/menu.js
+++ src/script/menu.js
@@ -1,19 +1,19 @@
 // for create humburger menu
-const hamburgerIcon = document.getElementById('hamburger-menu');
-const menuList = document.getElementById('menu-list')
+const hamburgerIcon = document.getElementById("hamburger-menu");
+const menuList = document.getElementById("menu-list");
 
-hamburgerIcon.addEventListener('click', () => {
-    menuList.classList.toggle('active');
+hamburgerIcon.addEventListener("click", () => {
+  menuList.classList.toggle("active");
 });
 
 // function emenrgency call
-const emergencyCall = document.getElementById('emergencyCall');
+const emergencyCall = document.getElementById("emergencyCall");
 
-        if (emergencyCall) {
-            emergencyCall.style.cursor = 'pointer'
-            emergencyCall.addEventListener('click', function () {
-                const phoneNumber = '+6282112706323';
+if (emergencyCall) {
+  emergencyCall.style.cursor = "pointer";
+  emergencyCall.addEventListener("click", function () {
+    const phoneNumber = "+6282112706323";
 
-                window.location.href = 'tel:' + phoneNumber;
-            });
-        }
+    window.location.href = "tel:" + phoneNumber;
+  });
+}
